562da84d67f20c14e1a781ab1c6eceed4a3f9510
Nika_Miku <diademistv@gmail.com>
2024-02-07 00:13:53 +0300

    Rework

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,37 @@ import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static AtomicInteger count3 = new AtomicInteger(0);
-    public static AtomicInteger count4 = new AtomicInteger(0);
-    public static AtomicInteger count5 = new AtomicInteger(0);
-    public static void main(String[] args) throws InterruptedException {
+
+    private static AtomicInteger word3 = new AtomicInteger(0);
+    private static AtomicInteger word4 = new AtomicInteger(0);
+    private static AtomicInteger word5 = new AtomicInteger(0);
+
+    public static void main(String[] args) {
         Random random = new Random();
         String[] texts = new String[100_000];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
-        Thread thread3 = new Thread(() -> {
-            for (String text : texts) {
-                if(isPalindrome(text)) {
-                    count3.incrementAndGet();
-                }
-            }
-        });
-        Thread thread4 = new Thread(() -> {
-            for (String text : texts) {
-                if(isSameCharacter(text)) {
-                    count4.incrementAndGet();
-                }
-            }
-        });
-        Thread thread5 = new Thread(() -> {
-            for (String text : texts) {
-                if(isSorted(text)) {
-                    count5.incrementAndGet();
-                }
-            }
-        });
+
+        Thread thread1 = new Thread(() -> checkPalindromeWords(texts, 3, word3));
+        Thread thread2 = new Thread(() -> checkSameLetterWords(texts, 4, word4));
+        Thread thread3 = new Thread(() -> checkAscendingWords(texts, 5, word5));
+
+        thread1.start();
+        thread2.start();
         thread3.start();
-        thread4.start();
-        thread5.start();
-        thread3.join();
-        thread4.join();
-        thread5.join();
-        System.out.println("Красивых слов с длинной 3: " + count3.get() + " шт");
-        System.out.println("Красивых слов с длинной 4: " + count4.get() + " шт");
-        System.out.println("Красивых слов с длинной 5: " + count5.get() + " шт");
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Красивых слов с длиной 3: " + word3);
+        System.out.println("Красивых слов с длиной 4: " + word4);
+        System.out.println("Красивых слов с длиной 5: " + word5);
     }
 
     public static String generateText(String letters, int length) {
@@ -52,24 +44,43 @@ public class Main {
         return text.toString();
     }
 
-    public static boolean isPalindrome(String text) {
-        StringBuilder reversedText = new StringBuilder(text).reverse();
-        return text.equals(reversedText.toString());
-    }
-
-    public static boolean isSameCharacter(String text) {
-        char firstChar = text.charAt(0);
-        for (int i = 0; i < text.length(); i++) {
-            if(text.charAt(i) != firstChar) {
+    public static boolean isPalindrome(String word) {
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - 1 - i)) {
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean isSorted(String text) {
-        for (int i = 1; i < text.length(); i++) {
-            if (text.charAt(i) < text.charAt(i - 1)) {
+    public static void checkPalindromeWords(String[] texts, int length, AtomicInteger counter) {
+        for (String text : texts) {
+            if (text.length() == length && isPalindrome(text)) {
+                counter.incrementAndGet();
+            }
+        }
+    }
+
+    public static void checkSameLetterWords(String[] texts, int length, AtomicInteger counter) {
+        for (String text : texts) {
+            if (text.length() == length && text.matches("^(.)\\1*$")) {
+                counter.incrementAndGet();
+            }
+        }
+    }
+
+    public static void checkAscendingWords(String[] texts, int length, AtomicInteger counter) {
+        for (String text : texts) {
+            if (text.length() == length && isAscending(text)) {
+                counter.incrementAndGet();
+            }
+        }
+    }
+
+    public static boolean isAscending(String word) {
+        for (int i = 0; i < word.length() - 1; i++) {
+            if (word.charAt(i) > word.charAt(i + 1)) {
                 return false;
             }
         }
